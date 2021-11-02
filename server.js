a54c0336920d8e7d41bdee9f80dac1e17425267b
Ekaterina Budenis <katsiaryna.budzenis@exposit.com>
2021-11-02 09:29:06 +0300

    change

--- server.js
+++ server.js
@@ -17,32 +17,36 @@ app.listen(port, (err) => {
 
 app.post('/add-user', jsonParser, (request, response) => {
   const requestedUser = request.body;
-  console.log(requestedUser)
+
   if (!requestedUser) {
     response.status(400).send('Missing user data');
   } else {
     fs.readFile('db.json', 'utf8', (err, data) => {
       if (err) {
+        console.log('error')
         return response.status(500).send(err);
       } else {
         try {
           const db = JSON.parse(data);
-          const users = db.users;
+          const users = db.users.sort((a, b) => a.id - b.id);
           const userExists = users.some((user) => user.email === requestedUser.email);
-
           if (userExists) {
             return response.status(400).json('User already exists');
           } else {
+            requestedUser.id = users[users.length - 1].id + 1;
+            requestedUser.userGames = [];
             users.push(requestedUser);
             json = JSON.stringify(db, null, 2, '\t');
             fs.writeFile('db.json', json, 'utf8', (err) => {
               if (err) {
+                console.log('error 1')
                 return response.status(500).send(err);
               }
               return response.status(200).json('OK');
             });
           }
         } catch (err) {
+          console.log('error 3')
           return response.status(500).send(err);
         }
       }
@@ -60,7 +64,7 @@ app.patch('/users/:email', jsonParser, (request, response) => {
         const users = db.users;
         users.map(item=> {
           if (item.email === request.params.email){
-              item.userGames.push(request.body)
+            item.userGames.push(request.body)
           }
         })
         json = JSON.stringify(db, null, 2, '\t');
@@ -82,7 +86,17 @@ app.get('/users', (request, response) => {
       try {
         const db = JSON.parse(data);
         const users = db.users;
-        return response.send(users);
+        const userInfo = users.map(user => {
+          return {
+            name: user.name,
+            surname: user.surname,
+            age: user.age,
+            email: user.email,
+            userGames: user.userGames,
+            id: user.id
+        }
+        })
+        return response.send(userInfo);
       } catch (err) {
         return response.status(500).send(err);
       }
